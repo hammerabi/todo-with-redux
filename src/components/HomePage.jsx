1690a015aaaa66c6fc2f2895fc062c8f52b0f481
josh <josh@bravenew.world>
2017-05-06 01:33:52 -0300

    beautify the jsx a wee bit

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -14,41 +14,41 @@ class HomePage extends React.Component {
   render() {
     return (
       <div>
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12">
-           <h1>Todo-Redux</h1>
-            <form onSubmit={e => {
-               e.preventDefault();
-               this.props.postNewTask(e.target.taskName.value);
-               e.target.taskName.value = "";
-              }
-             }>
-              <div className="form-group">
-              <label for="exampleInputEmail1">Add New To-Do</label>
-              <input name="taskName" placeholder="Enter new task" />
-              </div>
-              <button type="submit">Add</button>
-            </form>
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12">
+             <h1>Todo-Redux</h1>
+              <form onSubmit={e => {
+                 e.preventDefault();
+                 this.props.postNewTask(e.target.taskName.value);
+                 e.target.taskName.value = "";
+                }
+               }>
+                <div className="form-group">
+                <label for="exampleInputEmail1">Add New To-Do</label>
+                <input name="taskName" placeholder="Enter new task" />
+                </div>
+                <button type="submit">Add</button>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="container">
-        <div className="row">
-          <div className="col-xs-12">
-            <h3>Your To-do list</h3>
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-12">
+              <h3>Your To-do list</h3>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="container">
-        {
-          this.props.tasks && this.props.tasks.map((task) => {
-            return (
-              <Task key={task._id} Obj={task} isComplete={task.metafields[0].value} Name={task.title}/>
-            )
-          })
-        }
-      </div>
+        <div className="container">
+          {
+            this.props.tasks && this.props.tasks.map((task) => {
+              return (
+                <Task key={task._id} Obj={task} isComplete={task.metafields[0].value} Name={task.title}/>
+              )
+            })
+          }
+        </div>
       </div>
     )
   }
